446d7627a0d9c0f2c1294a1a594e8d09a9b9f5b1
simzrg <simz@niyash.co.za>
2024-11-28 07:12:28 +0200

    Update app_image_alt_latex.py

--- app_image_alt_latex.py
+++ app_image_alt_latex.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
-# Page configuration must come first
-st.set_page_config(
-    page_title="LaTeX Generator",
-    page_icon="🖼️",
-    layout="centered",
-    initial_sidebar_state="expanded",
-)
-
 APP_URL = ""  # TODO: Add URL for the app
 APP_IMAGE = ""  # TODO: Add default image for the app
 PUBLISHED = False  # Status of the app
